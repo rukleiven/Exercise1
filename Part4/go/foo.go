321b57d281ed07fd9bf8ce1dbb0fdc72ae0b7d63
Stineaak <46577497+Stineaak@users.noreply.github.com>
2019-01-18 09:58:01 +0100

    Update foo.go

--- Part4/go/foo.go
+++ Part4/go/foo.go
@@ -11,11 +11,15 @@ import (
 var i = 0
 
 func incrementing() {
-    //TODO: increment i 1000000 times
+    for j := 0; j < 1000000; j++ {
+		i += 1
+	}
 }
 
 func decrementing() {
-    //TODO: decrement i 1000000 times
+	for j := 0; j < 1000000; j++ {
+		i -= 1
+	}
 }
 
 func main() {
@@ -23,6 +27,8 @@ func main() {
 	                                    // Try doing the exercise both with and without it!
 
     // TODO: Spawn both functions as goroutines
+	go incrementing()
+	go decrementing()
 	
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
